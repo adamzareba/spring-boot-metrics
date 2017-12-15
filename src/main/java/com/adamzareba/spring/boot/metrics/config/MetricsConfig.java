75587c746347d99345f5b355ed0e6bc20071cbaa
Adam Zaręba <adam.zareba@softwarehut.pl>
2017-12-15 14:17:09 +0100

    y

--- src/main/java/com/adamzareba/spring/boot/metrics/config/MetricsConfig.java
+++ src/main/java/com/adamzareba/spring/boot/metrics/config/MetricsConfig.java
@@ -1,42 +1,50 @@
 package com.adamzareba.spring.boot.metrics.config;
 
-import org.influxdb.InfluxDB;
-import org.influxdb.dto.Point;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+import com.readytalk.metrics.StatsDReporter;
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
-import org.springframework.boot.actuate.endpoint.MetricsEndpointMetricReader;
-import org.springframework.boot.actuate.metrics.writer.GaugeWriter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-public class MetricsConfig {
+@EnableMetrics
+public class MetricsConfig extends MetricsConfigurerAdapter {
 
-    @Autowired
-    private InfluxDB influxDB;
+    @Value("${statsD.host}")
+    public String hostIP;
 
-    @Value("${spring.influx.database}")
-    private String database;
+    @Value("${statsD.port}")
+    public int port;
 
-    @Bean
-    @ExportMetricWriter
-    public GaugeWriter influxMetricsWriter() {
-        influxDB.setDatabase(database);
-        influxDB.enableBatch(10, 1000, TimeUnit.MILLISECONDS);
+    @Value("${statsD.period}")
+    public int period;
 
-        return (metricValue) -> {
-            Point point = Point.measurement(metricValue.getName()).time(metricValue.getTimestamp().getTime(), TimeUnit.MILLISECONDS)
-                    .addField("value", metricValue.getValue()).build();
-            influxDB.write(point);
-        };
+    @Value("${statsD.prefix}")
+    public String prefix;
+
+    @Override
+    public void configureReporters(MetricRegistry metricRegistry) {
+        StatsDReporter reporter = getStatsDReporterBuilder(metricRegistry).build(hostIP, port);
+        registerReporter(reporter);
+        reporter.start(period, TimeUnit.SECONDS);
     }
 
-    @Bean
-    public MetricsEndpointMetricReader metricsEndpointMetricReader(final MetricsEndpoint metricsEndpoint) {
-        return new MetricsEndpointMetricReader(metricsEndpoint);
+    private StatsDReporter.Builder getStatsDReporterBuilder(MetricRegistry metricRegistry) {
+        metricRegistry.register("gc", new GarbageCollectorMetricSet());
+        metricRegistry.register("memory", new MemoryUsageGaugeSet());
+        metricRegistry.register("threads", new ThreadStatesGaugeSet());
+        return StatsDReporter.forRegistry(metricRegistry)
+                .prefixedWith(prefix)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .filter(MetricFilter.ALL);
     }
 }
